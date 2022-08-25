d509abb4f9b4dd34259962c4849ffbe65cefa764
Jimi Syrjä <95481040+JimiSyrja@users.noreply.github.com>
2022-08-25 08:55:09 +0000

    2 moving balls

--- script.js
+++ script.js
@@ -3,23 +3,29 @@ function setup() {
   background(133, 193, 233)
 }
 
+
+let a = 1 
+let b = 7
+let c = 400
+let d = 400
 let y = 0
 let x = 0
 let v = 5
 let z = 3
+
 function draw() {
   
   background(133, 193, 233)
   if(mouseIsPressed){
-    ellipse(mouseX, mouseY, 100, 100)
     fill(230, 126, 34);
+    ellipse(mouseX, mouseY, 100, 100)  
   } else {
-  fill(39, 174, 96)
+  fill(255, 255, 255)
   rect(150, 150, 80, 80);
   }
   
-
-  ellipse(x,y,80,80)
+  fill(0, 0, 255)
+  ellipse(x,y,100,100)
   x = x+v;
   y = y+z;
 
@@ -37,5 +43,26 @@ function draw() {
 
   if(y<0){
     z = 3
+  }
+
+  fill(255, 0, 0)
+  ellipse(c,d,75,75)
+  c = c + a;
+  d = d + b;
+
+  if (c>400){
+     a = -1
+  }
+  
+  if(c<0){
+    a = 1
+  }
+
+  if(d>400){
+    b = -2
+  }
+
+  if(d<0){
+    b = 2
   }
 }
