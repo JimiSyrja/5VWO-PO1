a1f45c00a3f58158cef6a0439190873fe98e2451
Jimi Syrjä <95481040+JimiSyrja@users.noreply.github.com>
2022-08-31 12:04:43 +0000

    4 bouncing balls with "class"

--- script.js
+++ script.js
@@ -1,68 +1,49 @@
+class ball{
+  constructor(x, y, w, h, vx, vy, c){
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.vx = vx;
+    this.vy = vy;
+    this.color = c;
+  }
+
+  draw(){
+    fill(this.color)
+    ellipse(this.x,this.y,this.width,this.height);
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+     if(this.x < 0 || this.x >= 400){
+      this.vx = this.vx * -1;
+    }
+
+    if(this.y < 0 || this.y >= 400){
+      this.vy = this.vy * -1;
+    }
+  }
+}
+
+var ball1, ball2, ball3, ball4, ball5;
+
 function setup() {
   createCanvas(400, 400);
   background(133, 193, 233)
+
+  ball1 = new ball(70,30,50,50,5,5,"red");
+  ball2 = new ball(375,20,50,50,6,6,"green")
+  ball3 = new ball(100,100,50,50,2,4,"yellow")
+  ball4 = new ball(254,326,50,50,4,2,"orange")
 }
 
 
-let a = 1 
-let b = 7
-let c = 400
-let d = 400
-let y = 0
-let x = 0
-let v = 5
-let z = 3
 
 function draw() {
+	background(133,193,233);  
   
-  background(133, 193, 233)
-  if(mouseIsPressed){
-    fill(230, 126, 34);
-    ellipse(mouseX, mouseY, 100, 100)  
-  } else {
-  fill(255, 255, 255)
-  rect(150, 150, 80, 80);
-  }
-  
-  fill(0, 0, 255)
-  ellipse(x,y,100,100)
-  x = x+v;
-  y = y+z;
-
-  if (x>400){
-     v = -5
-  }
-  
-  if(x<0){
-    v = 5
-  }
-
-  if(y>400){
-    z = -3
-  }
-
-  if(y<0){
-    z = 3
-  }
-
-  fill(255, 0, 0)
-  ellipse(c,d,75,75)
-  c = c + a;
-  d = d + b;
-
-  if (c>400){
-     a = -1
-  }
-  
-  if(c<0){
-    a = 1
-  }
-
-  if(d>400){
-    b = -2
-  }
-
-  if(d<0){
-    b = 2
-  }
+  ball1.draw();
+  ball2.draw();
+  ball3.draw();
+  ball4.draw();
 }
